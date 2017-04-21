3a830f9f7fc68dbbd1d964b23ed33c33acdc26dd
André Lucas <andre.lucas@storageos.com>
2017-04-21 17:27:21 +0100

    Auto-shutdown script
    Saves money at DigitalOcean.

---
+++ shutdown.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e -u
+
+. test.env
+
+hosts=""
+
+# IPs of the test nodes.
+for p in $PREFIX $PREFIX2 $PREFIX3; do
+    ip=$(echo "$p" | perl -ne 'print "$1\n" if m/\w+@([0-9.]+)$/;')
+    hosts="$hosts $ip"
+done
+# IP of the KV server.
+ip=$(echo $KV_ADDR | perl -ne 'print "$1\n" if m/([0-9.]+)(:\d+)?$/;')
+hosts="$hosts $ip"
+
+# Formatting OCD.
+hosts="$(echo $hosts | tr -s ':space:')"
+
+echo "Hosts: $hosts"
+for h in $hosts; do
+    echo "Checking $h"
+    if nc -zv -G3 "$h" 22; then
+        echo "** Shutting down $h"
+        ssh "root@$h" poweroff || true
+    else
+        echo "-- Skipping $h"
+    fi
+done
