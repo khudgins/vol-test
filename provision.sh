5b22d3a078dfb77fd4d991f49e82c57d6e016c40
André Lucas <andre@ae-35.com>
2017-08-09 18:15:08 +0100

    More characters for tags/names (#14)
    * Shellcheck; Remove KV_ADDR; reduce volume
    
    * More characters in the name/tag
    
    Otherwise feature/dev-1234 will be the same tag as feature/dev-4321,
    which is silly
    
    * Use nice-looking subset of uuid.

--- provision.sh
+++ provision.sh
@@ -8,11 +8,11 @@ version="${VERSION:-latest}"
 cli_version="${CLI_VERSION:-0.0.11}"
 
 branch_env="${BRANCH_ENV:-branchnotset}"
-# Make sure branch_env has at most ten characters.
+# Make sure branch_env has at most 16 characters.
 if [ "$branch_env" = "branchnotset" ]; then
-    branch_env="$(uuidgen | cut -c1-8)"
+    branch_env="$(uuidgen | cut -c1-13)"
 else
-    branch_env="$(echo "$branch_env" | cut -c1-10)"
+    branch_env="$(echo "$branch_env" | cut -c1-16)"
 fi
 
 build_id="${branch_env}-${BUILD:-buildnotset}"
