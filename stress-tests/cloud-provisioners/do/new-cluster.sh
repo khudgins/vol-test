d01c56fc82276695e372d29c6eb464cb6b51f070
Houssem El Fekih <houssem.elfekih@storageos.com>
2017-09-06 17:46:02 +0100

    need to clarify requirements

--- stress-tests/cloud-provisioners/do/new-cluster.sh
+++ stress-tests/cloud-provisioners/do/new-cluster.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -ex
 
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # TODO: provision new key and add to jenkins slaves
 
-if [[ -z $Suite ]] || [[ -z "$STORAGEOS_CONTAINER_VERSION" ]]; then
+if [[ -z $SUITE ]] || [[ -z "$STORAGEOS_VERSION" ]]; then
   (>2& echo "Please specify the Job you want to run and the container version") 
+  exit 1
 fi
 
-JOBUID=$Suite-$(echo $STORAGEOS_CONTAINER_VERSION | tr '.' '_')
+JOBUID=$SUITE-$(echo $STORAGEOS_VERSION | tr '.' '_')
 
 set +x
-find . -type f -name $JOBUID
-if [[ $? -eq 0 ]]; then
+
+if [[ -f "$PROJECT_DIR/$JOBUID.txt" ]]; then
   (>2& echo "This job already exists, cancel through cancel job to restart") 
   exit 1
 fi  
 set -x
 
-VERSION=$STORAGEOS_VERSION JOBUID=$JOBUID lib/bash-templater/templater.sh cluster.template > $JOBUID.tf
+VERSION=$STORAGEOS_VERSION JOBUID=$JOBUID $PROJECT_DIR/lib/bash-templater/templater.sh cluster.template > $JOBUID.tf
 
 terraform plan -var "tag=stress"  \
   -var "do_token=4ebbb814ce4d4edb19f4a8c410cdf2944fd74b110f10e5650030bc3802e9a0cb" \
--- stress-tests/stress-test-trigger.sh
+++ stress-tests/stress-test-trigger.sh
@@ -2,11 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+. envfile.sh
+
 for IaaS in $IAAS; do
 
   for Suite in $SUITES; do
 
-    SUITE=$Suite $DIR/cloud-provisioners/${IaaS}/new-machine.sh
+    env SUITE=$Suite STORAGEOS_VERSION=$STORAGEOS_VERSION $DIR/cloud-provisioners/${IaaS}/new-cluster.sh
     if [[ $? -ne 0 ]]; then
       echo "Provisioner failed for suite $Suite, continuing.."
       continue 
